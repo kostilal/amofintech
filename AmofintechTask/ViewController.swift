b033bbf2de6dc1e897d16518306c49ed1459a0f6
Illia Kostiukevych <i.kostiukevych@smartweb.com.ua>
2024-02-07 11:37:47 +0100

    Initial Commit

---
+++ AmofintechTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AmofintechTask
+//
+//  Created by Illia Kostiukevych on 07.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
